bd879e3aeb37d12d0396d7048ad496fe9b59373d
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-07-31 13:11:16 -0700

    Index.js for schemas

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schemas');
+const { typeDefs, resolvers } = require('./Schemas');
 const { authMiddleware } = require('./utils/auth');
 
 const app = express();
