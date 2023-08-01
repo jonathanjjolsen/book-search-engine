c4b5b3246bb4df29e26c3ba0debe204bec307c59
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-08-01 12:18:15 -0700

    Added Queries.js

---
+++ client/src/utils/queries.js
@@ -0,0 +1,22 @@
+import gql from 'graphql-tag';
+
+export const QUERY_ME = gql`
+
+{
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                # _id
+                bookId
+                authors
+                image
+                link
+                title
+                description
+            }
+        }
+    }
+`;
--- server/config/connection.js
+++ server/config/connection.js
@@ -6,5 +6,4 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
     useUnifiedTopology: true,
 });
 
-
 module.exports = mongoose.connection;
