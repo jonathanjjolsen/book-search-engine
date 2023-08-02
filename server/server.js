76b34355f6e12d2609085b907422fea0c86668b9
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-08-01 23:45:21 -0700

    Going in a different direction

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./Schemas');
 const { authMiddleware } = require('./utils/auth');
@@ -17,7 +17,6 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(routes);
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -28,11 +27,11 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 })
 
-const startServer = async () => {
+const startServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
   db.once('open', () => {
     app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
   })};
 
-startServer();
+startServer(typeDefs, resolvers);
