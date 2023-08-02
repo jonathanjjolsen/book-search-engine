f700498b0b460df172e4016e8e4aeaebee3678d7
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-08-02 00:04:39 -0700

    Modified Resolvers.js

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -6,42 +6,60 @@ const resolvers = {
     Query: {
         me: async (parent, args, context) => {
             if (context.user) {
-                const userData = await User.findOne({ _id: context.user._id })
-                    .select('-__v -password')
-                return userData;
+              return User.findOne({ _id: context.user._id });
             }
-            throw new AuthenticationError('Not logged in');
-        }
+            throw new AuthenticationError('You need to be logged in!');
+          },
     },
     Mutation: {
         login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
-            if (!user) {
-                throw new AuthenticationError('Incorrect credentials');
-            }
-            const correctPw = await user.isCorrectPassword(password);
-            if (!correctPw) {
-                throw new AuthenticationError('Incorrect credentials');
-            }
-            const token = signToken(user);
-            return { token, user };
+          const user = await User.findOne({ email });
+    
+          if (!user) {
+            throw new AuthenticationError('No user with this email found!');
+          }
+    
+          const correctPw = await user.isCorrectPassword(password);
+    
+          if (!correctPw) {
+            throw new AuthenticationError('Incorrect password!');
+          }
+    
+          const token = signToken(user);
+          return { token, user };
         },
-
-        addUser: async (parent, args) => {
-            const user = await User.create(args);
-            const token = signToken(user);
-            return { token, user };
+        addUser: async (parent, { username, email, password }) => {
+          const user = await User.create({ username, email, password });
+          const token = signToken(user);
+          
+          return { token, user};
         },
-        saveBook: async (parent, { input }, context) => {
-            if (context.user) {
-                const updatedUser = await User.findByIdAndUpdate(
-                    { _id: context.user._id },
-                    { $push: { savedBooks: input } },
-                    { new: true }
-                );
-                return updatedUser;
-            }
-            throw new AuthenticationError('You need to be logged in!');
+        saveBook: async (parent, book, context) => {
+          if (context.user) {
+            return User.findOneAndUpdate(
+              { _id: context.user._id},
+              {
+                $addToSet: { savedBooks: book},
+              },
+              {
+                new: true,
+                runValidators: true 
+              }
+            );
+          }
+          throw new AuthenticationError('You need to be logged in!');
         },
-    }
+        removeBook: async (parent, { bookId }, context) => {
+          if (context.user) {
+            return User.findOneAndUpdate(
+              { _id: context.user._id },
+              { $pull: { savedBooks: { bookId: bookId } } },
+              { new: true }
+            );
+          }
+          throw new AuthenticationError('You need to be logged in!');
+        },
+      },
 };
+
+module.exports = resolvers;
