8db050920de26e872444adc63ed6eebd588b8c2b
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-08-01 23:24:37 -0700

    Modified App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,27 +4,55 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+//importing ApolloClient and ApolloProvider
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+//establishing new link to the GraphQL server at its /graphql endpoint using createHttpLink
+const httpLink = createHttpLink({
+  uri: '/graphql'
+});
+
+//creating a new middleware function to retrieve the token from localStorage and set the HTTP request headers of every request to include the token
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
+//creating a new instance of ApolloClient and passing in the authLink middleware function to add the token to every request sent to the server
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Routes>
-          <Route 
-            path='/' 
-            element={<SearchBooks />} 
-          />
-          <Route 
-            path='/saved' 
-            element={<SavedBooks />} 
-          />
-          <Route 
-            path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
-          />
-        </Routes>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Routes>
+            <Route
+              path='/'
+              element={<SearchBooks />}
+            />
+            <Route
+              path='/saved'
+              element={<SavedBooks />}
+            />
+            <Route
+              path='*'
+              element={<h1 className='display-2'>Wrong page!</h1>}
+            />
+          </Routes>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
