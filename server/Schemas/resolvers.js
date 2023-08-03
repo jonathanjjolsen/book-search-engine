cc70ce91ac3fc324302066b36fc6ecdff1c8cfa0
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-08-03 13:48:54 -0700

    Added error to catch

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -29,10 +29,16 @@ const resolvers = {
           return { token, user };
         },
         addUser: async (parent, { username, email, password }) => {
-          const user = await User.create({ username, email, password });
+          try{
+            const user = await User.create({ username, email, password });
           const token = signToken(user);
           
           return { token, user };
+          } catch (err) {
+            console.log(err);
+            throw err;
+          }
+
         },
         saveBook: async (parent, { input }, context) => {
           if (context.user) {
